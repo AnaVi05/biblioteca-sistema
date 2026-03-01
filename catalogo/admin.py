e64634ed4a16a3cdca28d3d742350cd950c15fd9
belen371 <belenvillalba809@gmail.com>
2026-03-01 16:49:00 -0300

    Agrega modelo Editorial y migraciones

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Autor,Editorialc
 
-# Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'apellido')
+    search_fields = ('nombre', 'apellido')
+
+@admin.register(Editorial)
+class EditorialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('nombre',)
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    """Autores de los libros"""
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+    
+    @property
+    def nombre_completo(self):
+        return f"{self.nombre} {self.apellido}"
+class Editorial(models.Model):
+    """Editoriales de los libros"""
+    nombre = models.CharField(
+        max_length=100, 
+        unique=True, 
+        verbose_name="Nombre de la editorial"
+    )
+    
+    class Meta:
+        verbose_name = "Editorial"
+        verbose_name_plural = "Editoriales"
+    
+    def __str__(self):
+        return self.nombre
